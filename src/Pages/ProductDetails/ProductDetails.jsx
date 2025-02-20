441378bc99c129db46bcaf2a36818d3052d75c90
iramywb <iramywb@gmail.com>
2025-02-20 18:00:05 +0200

    Fix Import

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -7,7 +7,7 @@ import Loader from "../../Components/Loader/Loader";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import style from "./ProductDetails.module.css";
+import "./ProductDetails.module.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 import { WishlistContext } from "../../Context/WishlistContext";
